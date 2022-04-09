5da7ada5484092ee4119cddb6cc94a5a6a16cb10
Rifat Jamil <rifatjamil54@gmail.com>
2022-04-09 14:57:28 +0600

    cmt

---
+++ pass or call object referrrence by python.py
@@ -0,0 +1,17 @@
+def vat(x):
+    x = 15
+    print(x, id(x))
+x = 10
+vat(x)
+print(x, id(x))
+
+
+
+def val(p):
+    lis.append(4)
+    print(lis, id(lis))
+    
+lis = [1, 2, 3]
+print(lis, id(lis)) 
+   
+val(lis)
