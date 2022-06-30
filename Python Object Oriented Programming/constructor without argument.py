c4e5c2cd01e9cf30f6a3418b8f82007af5a36abd
rifatjamil54 <rifatjamil54@gmail.com>
2022-06-30 09:37:47 +0600

    commit

--- Python Object Oriented Programming/constructor without argument.py
+++
@@ -1,9 +0,0 @@
-class Mobile:
-    def __init__(self):
-        self.model = "Realme X"
-
-    def show_model(self):
-        print("Model:",self.model)    
-
-realme = Mobile()        
-realme.show_model()
